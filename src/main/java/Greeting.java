ce24b98b2b751cd4fabef8f7072375c4c68ca69b
Rahul <jain_rahul@ymail.com>
2020-04-28 23:05:49 -0400

    usage of Funtional Interface Annotation

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -1,4 +1,8 @@
+// Functional Interface Annnotation Enforces that we can't add Multiple methods to the Interfaces. This Interface is used for Lambda Expression
+@FunctionalInterface
 public interface Greeting {
 
     public String Perform();
+
+    //public String anotherMethod();
 }
